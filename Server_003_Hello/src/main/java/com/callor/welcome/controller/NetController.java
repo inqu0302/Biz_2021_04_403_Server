3f3ad6d9f6213f08b5b2b2dc0824e6f17f8825c1
inqu0302 <inqu0302@naver.com>
2021-04-28 16:41:43 +0900

    JSPcording

---
+++ Server_003_Hello/src/main/java/com/callor/welcome/controller/NetController.java
@@ -0,0 +1,24 @@
+package com.callor.welcome.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/inqu0302")
+public class NetController extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		
+		out.print("<h1>Hello my Home</h1>");
+		out.close();
+	}
+	
+	
+}
